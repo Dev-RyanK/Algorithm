696a1634e7adce99bf510b4c5e4e22dd7f03c3c7
Dev Ryan Kwon <103974022+Dev-RyanK@users.noreply.github.com>
2022-08-04 23:11:26 +0900

    Add files via upload

---
+++ 1-입출력과 사칙연산/220802-2557-10172.js
@@ -0,0 +1,16 @@
+console.log('Hello World!');
+console.log('강한친구 대한육군');
+console.log('강한친구 대한육군');
+
+// 역슬래시는 두 개로 표현
+console.log("\\    /\\");
+console.log(" )  ( ')");
+console.log("(  /  )");
+console.log(" \\(__)|");
+
+// 쌍따옴표에 주의
+console.log('|\\_/|')
+console.log('|q p|   /}')
+console.log('( 0 )"""\\')
+console.log('|"^"`    |')
+console.log('||_/=\\\\__|')
