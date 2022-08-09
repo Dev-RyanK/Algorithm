ce2390e7c1fa24b6ced3ff5fc571b8e4208e8667
Dev Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-08-09 11:05:36 +0900

    map 이용해서 배열 안 내용 사칙연산

--- 1-입출력과 사칙연산/220808-3003.js
+++ 1-입출력과 사칙연산/220808-3003.js
@@ -1,12 +1,14 @@
-// let fs = require('fs');
-// let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
 
+input = [0, 0, 0, 0, 2, 8];
+// 배열 지정
 const chess = [1, 1, 2, 2, 2, 8];
-let answer = input.map((i, index) => {
-  return chess[index] - i;
-})
+// map: 배열 내의 모든 요소 각각에 대하여 주어진 함수를 호출한 결과를 모아 새로운 배열을 반환하는 메서드
+// let answer = input.map(function(i, index){
+//   return chess[index] - i
+// });
+let answer = input.map((i, index) => chess[index] - i);
 
+// (answer) 로 쓰면 배열 자체를 보여주고 ...을 쓰면 내용물을 보여준다
 console.log(...answer);
-
-
-// 미루지 말고 하나하나 뜯어볼 것
