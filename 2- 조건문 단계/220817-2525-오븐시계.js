2932aa29c88e4ebab65f4bede8e1add639d164c9
Dev Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-08-18 11:13:46 +0900

    split('\n') 한 뒤 다시 split(' ') 해서 두 줄짜리 연산

--- 2- 조건문 단계/220817-2525-오븐시계.js
+++ 2- 조건문 단계/220817-2525-오븐시계.js
@@ -1,2 +1,16 @@
 let fs = require('fs');
-let input = fs.readFileSync(/dev/stdin).toString().trim().split(' ').map(Number);
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = fs.readFileSync(__dirname + '\\ex.txt').toString().trim().split('\n');
+
+// input을 \n 으로 나눠서, [0]은 '시간 분', [1]은 '동작 시간'. 변수 선언을 해주는 게 Number(input[1])보다는 짧게 먹힘.
+
+const current = input[0].split(' ').map(Number);
+
+const currentHour = current[0];
+const currentMin = current[1];
+const cookTime = Number(input[1]);
+
+const cookEndTimeHour = parseInt((currentHour * 60 + currentMin + cookTime) / 60);
+const cookEndTimeMin = parseInt((currentHour * 60 + currentMin + cookTime) % 60);
+
+console.log(cookEndTimeHour >= 24 ? cookEndTimeHour - 24 : cookEndTimeHour, cookEndTimeMin);
