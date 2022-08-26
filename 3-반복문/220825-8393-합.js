0f1f57d5a364264e046f5ef1e20e28849b0a44c6
DESKTOP-EE1VRFB\my <dev.ryan.kwon@gmail.com>
2022-08-26 22:49:49 +0900

    for문 사용 1부터 input까지의 합을 구한다

--- 3-반복문/220825-8393-합.js
+++ 3-반복문/220825-8393-합.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
-// const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+// const input = fs.readFileSync('/dev/stdin').toString();
 const input = fs.readFileSync(__dirname + '\\ex.txt').toString();
 
-let output = 1
+let num = 0;
 for (let i = 1; i <= input; i++) {
-  output += i
+  num += i
 }
+
+console.log(num);
