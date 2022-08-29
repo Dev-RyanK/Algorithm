e2f29ba5d4ba8cf74eacd70c3b91377fb5673cf5
DESKTOP-EE1VRFB\my <dev.ryan.kwon@gmail.com>
2022-08-29 19:31:02 +0900

    .

--- 3-반복문/220827-15552-빠른A+B.js
+++ 3-반복문/220827-15552-빠른A+B.js
@@ -1,3 +1,14 @@
 const fs = require('fs');
-// const input = fs.readFileSync('/dev/stdin').toString()
-const input = fs.readFileSync(__dirname + '\\ex.txt').toString()
+// const input = fs.readFileSync('/dev/stdin').toString().split('\n')
+const input = fs.readFileSync(__dirname + '\\ex.txt').toString().split('\n')
+
+let total = input[0]
+let sum = input[1]
+let answer = 0;
+
+for (i = 2; i < input[0]; i++) {
+  let newArr = input[i].split(' ').map(item => Number(item)); //화살표 함수의 사용 이유? item의 사용법은?
+  answer = newArr[0] + newArr[1];
+}
+
+console.log(answer);
