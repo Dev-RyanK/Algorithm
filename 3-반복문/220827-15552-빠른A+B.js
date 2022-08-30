b764311b5ebd6cd440766e66dbd834d98bd670a2
Dev Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-08-30 20:35:27 +0900

    .

--- 3-반복문/220827-15552-빠른A+B.js
+++ 3-반복문/220827-15552-빠른A+B.js
@@ -2,13 +2,12 @@ const fs = require('fs');
 // const input = fs.readFileSync('/dev/stdin').toString().split('\n')
 const input = fs.readFileSync(__dirname + '\\ex.txt').toString().split('\n')
 
-let total = input[0]
-let sum = input[1]
-let answer = 0;
+let max = Number(input[0]);
+let answer = '';
 
-for (i = 2; i < input[0]; i++) {
-  let newArr = input[i].split(' ').map(item => Number(item)); //화살표 함수의 사용 이유? item의 사용법은?
-  answer = newArr[0] + newArr[1];
+for (let i = 1; i <= max; i++) {
+  let num = input[i].split(' ');
+  answer += Number(num[0]) + Number(num[1]) + "\n";
 }
 
 console.log(answer);
