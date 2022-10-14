caea81b9efb86154d7ee756f40ac5d7ec91bee83
DESKTOP-EE1VRFB\my <dev.ryan.kwon@gmail.com>
2022-10-14 14:29:22 +0900

    Set(map(x=>x%42)).size

--- 4-1차원 배열/221013-3052-나머지.js
+++ 4-1차원 배열/221013-3052-나머지.js
@@ -1,13 +1,31 @@
 const fs = require('fs')
-// const input = fs.readFileSync('/dev/stdin').toString().split('\n')
-const input = fs.readFileSync(__dirname + '\\ex.txt').toString().split('\n')
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+const input = fs.readFileSync(__dirname + '\\ex.txt').toString().trim().split('\n')
 
-// console.log(input);
-let temp = 0
-for (i = 0; i < 10; i++) {
-  // let remainder = input[i] % 42
+/* // 틀린 코드
+let temp = []
+let tempCount = 0
+for (i = 0; i < input.length; i++) {
   if (input[i] % 42 !== input[i + 1] % 42) {
-    temp += 1
+    temp.push(input[i] % 42)
+    temp.sort()
+  }
+  if (temp[i] !== temp[i + 1]) {
+    tempCount += 1
   }
 }
-console.log(temp);
+console.log(tempCount); */
+
+// 내 코드
+let output = []
+for (i = 0; i < input.length; i++) {
+  let remainder = input[i] % 42
+  output.push(remainder)
+}
+const count = new Set(output).size
+
+console.log(count);
+
+// 배운 코드
+const count2 = new Set(input.map(x => x % 42)).size
+console.log(count2)
---
+++ 4-1차원 배열/221014-1546-평균.js
@@ -0,0 +1,5 @@
+// https://www.acmicpc.net/problem/1546
+
+let input = require('fs').readFileSync(__dirname + '\\ex.txt').toString().split('\n')
+// let input = require('fs').readFileSync('/dev/stdin').toString().split('\n')
+
