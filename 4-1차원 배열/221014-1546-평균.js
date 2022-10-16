101156685f9cd585b3901802ecaf112d6978b761
DESKTOP-EE1VRFB\my <dev.ryan.kwon@gmail.com>
2022-10-16 22:54:39 +0900

    최대값 구하기, 배열 내 값 합치기

--- 4-1차원 배열/221014-1546-평균.js
+++ 4-1차원 배열/221014-1546-평균.js
@@ -3,3 +3,16 @@
 let input = require('fs').readFileSync(__dirname + '\\ex.txt').toString().split('\n')
 // let input = require('fs').readFileSync('/dev/stdin').toString().split('\n')
 
+let input2 = input[1].split(' ')
+let M = Math.max(...input2)
+let newScoreArray = []
+for (i = 0; i < input2.length; i++) {
+  let newScore = input2[i] / M * 100
+  newScoreArray.push(newScore)
+}
+let total = newScoreArray.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  0
+)
+let result = total / parseInt(input)
+console.log(result);
