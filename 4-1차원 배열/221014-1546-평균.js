4ea920e7af4376a747f8e5dea2080075f8c72a59
Dev Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-10-17 10:18:16 +0900

    no message

--- 4-1차원 배열/221014-1546-평균.js
+++ 4-1차원 배열/221014-1546-평균.js
@@ -3,7 +3,8 @@
 let input = require('fs').readFileSync(__dirname + '\\ex.txt').toString().split('\n')
 // let input = require('fs').readFileSync('/dev/stdin').toString().split('\n')
 
-let input2 = input[1].split(' ')
+// 내 코드
+/* let input2 = input[1].split(' ')
 let M = Math.max(...input2)
 let newScoreArray = []
 for (i = 0; i < input2.length; i++) {
@@ -15,4 +16,18 @@ let total = newScoreArray.reduce(
   0
 )
 let result = total / parseInt(input)
-console.log(result);
+console.log(result); */
+
+
+// 좋아보이는 남의 코드
+const num = input[0] // 과목의 갯수
+const score = input[1].split(' ')
+
+const max = Math.max(...score) // score에서 최대값 추출
+let sum = 0;
+
+for (let i = 0; i < num; i++) {
+  sum += score[i] / max * 100 // 각 과목을 최대값으로 나누고 100을 곱한 값을 과목의 갯수만큼 전부 더함
+}
+
+console.log(sum / num);
