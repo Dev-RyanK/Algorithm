e3b0e03c42b75de2249b7bc159928bbfc5b2fb85
Ryan K <dev.ryan.kwon@gmail.com>
2022-11-20 16:24:55 +0900

    Add temporary file for questions in progress

--- test.js
+++ test.js
@@ -1,4 +1,13 @@
-function solution(participant, completion) {
-  var answer = '';
-  return answer;
+function solution(s) {
+  let arr = Array(...s.split(' '))
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i].length % 2 !== 0) {
+      let hey = arr[i]
+      return hey
+    }
+  }
+  // return arr
 }
+
+
+console.log(solution("try hello world"))
