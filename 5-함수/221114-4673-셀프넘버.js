12932298e827e3f0bb731891cd25bdfdebfd47ea
Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-11-28 09:55:32 +0900

    Add 4673 셀프넘버 (ongoing)

--- 5-함수/221114-4673-셀프넘버.js
+++ 5-함수/221114-4673-셀프넘버.js
@@ -1,7 +1,25 @@
-// 입력은 없다.
+/* let arr = [1, 3, 5, 7, 9, 20]
+for (let i = 1; i < 908; i++) {
+  console.log(20 + 11 * i)
+} */
+// 틀렸음! 끝자리 4 정도로 차이남
+// console.log(arr.length);
+// console.log(arr[arr.length - 1]);
 
-// 존나 이해를 못해서 일단 보류...
-let compare = Array.from({ length: 100 }, (v, i) => String(i + 1))
+let compare = Array.from({ length: 30 }, (v, i) => String(i + 1))
+let arr = []
+for (let i = 0; i < compare.length; i++) {
+  if (compare[i].length < 2) {
+    arr.push(Number(compare[i]) * 2)
+  } else {
+    // length === 2
+    for (let j = 0; j < compare[i].length; j++) {
+      // compare[i].map((v, idx) => (compare[i].length - i) + idx)
+      // compare[i] === ['10'] map ㅡ쓰려고 배열 선언함
+      let test = new Array(compare[i]).map((v, idx) => v[idx] + v[idx - j])
+      arr.push(Number(...test))
+    }
+  }
+}
 
-// if ()
-console.log(compare);
+console.log(arr);
