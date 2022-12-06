5e0bcdf2010bd516035f589e7e413a3d1c9c9bc6
Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-12-06 09:46:27 +0900

    Add 문자열 11654 아스키코드

---
+++ 6-문자열/11654-아스키코드.js
@@ -0,0 +1,15 @@
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim()
+const input = require("fs")
+  .readFileSync(__dirname + "\\ex.txt")
+  .toString()
+  .trim()
+
+console.log(input[0].charCodeAt(0))
+
+// 남의 풀이
+console.log(input.charCodeAt())
+
+// 남의 풀이2. 왜 이렇게 했을까?
+let index = 0
+const i = input.charCodeAt(index)
+console.log(i)
