684d9dfab17f35425e7f4011d5352ed57f9f7e1b
Dev-RyanK <dev.ryan.kwon@gmail.com>
2023-05-06 14:45:59 +0900

    feat: 25314 코딩은 체육과목입니다

---
+++ 3-반복문/220506-25314-체육과목.js
@@ -0,0 +1,8 @@
+// let input = require("fs").readFileSync("/dev/stdin").toString().trim()
+
+// let input = "long long long long int"
+let input = 20
+
+let N = "long ".repeat(input / 4) + "int"
+
+console.log(N)
