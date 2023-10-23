d317b0e664668ec3ca192483a2ae571c4d882eea
Dev-RyanK <dev.ryan.kwon@gmail.com>
2023-10-23 15:03:11 +0900

    feat: 주사위게임2

--- 프로그래머스/level0.js
+++ 프로그래머스/level0.js
@@ -283,14 +283,27 @@ let square = arr2.reduce((p, c) => p + c, Math.min(...arr2)) ** 2
 console.log(multiple, square)
 console.log(5 * 7 * 8 * 3) */
 
-// [미]주사위 게임2
-/* if (a !== b && b !== c && c !== a) {
-  return a + b + c
-} else if (a === b || b === c || c === a) {
-  return (a + b + c) * (a ** 2 + b ** 2 + c ** 2)
-} else if (a === b && b === c) {
-  return (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
-} */
+// 주사위 게임2
+/* const a1 = 2
+const b1 = 6
+const c1 = 1
+
+const a2 = 5
+const b2 = 3
+
+const a3 = 4
+
+function solution(a, b, c) {
+  if (a !== b && b !== c && c !== a) {
+    return a + b + c
+  } else if (a === b && a === c && b === c) {
+    return (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
+  } else return (a + b + c) * (a ** 2 + b ** 2 + c ** 2)
+}
+
+console.log(solution(a1, b1, c1))
+console.log(solution(a2, b2, b2))
+console.log(solution(a3, a3, a3)) */
 
 // flag에 따라 다른 값 반환하기
 /* return flag?a+b:a-b */
